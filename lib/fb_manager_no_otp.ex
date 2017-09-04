c5967cdb7eb406577ebf13526e6d80436003c2d6
Michael Hanna <mykelkim@yahoo.com>
2017-09-04 01:04:45 -0400

    add ffnerd dependency and compile

---
+++ lib/fb_manager_no_otp.ex
@@ -0,0 +1,18 @@
+defmodule FbManagerNoOtp do
+  @moduledoc """
+  Documentation for FbManagerNoOtp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FbManagerNoOtp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fb_manager_no_otp_test.exs
@@ -0,0 +1,8 @@
+defmodule FbManagerNoOtpTest do
+  use ExUnit.Case
+  doctest FbManagerNoOtp
+
+  test "greets the world" do
+    assert FbManagerNoOtp.hello() == :world
+  end
+end
